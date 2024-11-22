c464896badd23491f20f499e5d099901914a3325
swetha05-10-12 <iam.swethaashok@gmail.com>
2024-11-22 17:56:31 +0530

    Adding 3rd activity

---
+++ Lessons/Lesson-3/Activity-3.py
@@ -0,0 +1,9 @@
+print("Enter mraks obtained in 4 subjects:")
+math=int(input("Maths: "))
+english=int(input("English: "))
+science=int(input("Science: "))
+language=int(input("Language: "))
+sum= math+english+science+language
+print("You totally got:",sum,"marks")
+percentage=(sum/400)*100
+print("Your mark as a percentage is:",percentage)
