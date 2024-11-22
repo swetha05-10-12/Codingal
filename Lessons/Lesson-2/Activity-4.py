ee788873eb55edab32e80edde7f28588d3fa28d1
swetha05-10-12 <iam.swethaashok@gmail.com>
2024-11-22 16:32:45 +0530

    Adding 4th activity

---
+++ Lessons/Lesson-2/Activity-4.py
@@ -0,0 +1,23 @@
+name="Penguin"
+age=15
+is_student=True
+weight=38.5
+print("Name:", name)
+print("Data Type of Name is", type(name))
+
+print("Age :", age)
+print("Data Type of Age is", type(age))
+
+print("Is_Student :", is_student)
+print("Data Is_Student is", type(is_student))
+
+print("Weight :", weight)
+print("Data Type of Weight is", type(weight))
+
+print("After Type  Casting...")
+age=str(age)
+print(age)
+print("Data Type of Age is", type (age))
+weight= int(weight)
+print(weight)
+print("Data Type of Weighjt :", type(weight))
