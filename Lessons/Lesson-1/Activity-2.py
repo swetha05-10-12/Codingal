5a6dbbe2a467c3adf99c9c9ce2ce07b95e92a2d9
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-01-11 20:20:37 +0530

    Adding lesson 1

---
+++ Lessons/Lesson-1/Activity-2.py
@@ -0,0 +1,10 @@
+x=5
+y="John"
+print(x)
+print(y)
+
+codingal="Codingal"
+print(codingal)
+
+name=input("Enter your name: ")
+print("Hello",name,"welcome to Codingal!")
--- Lessons/Lesson-10/Activity-1.py
+++ Lessons/Lesson-10/Activity-1.py
@@ -1,5 +1,4 @@
 for j in range(5):
     for i in range(1,7):
-
         print(i,end=" ")
     print()
