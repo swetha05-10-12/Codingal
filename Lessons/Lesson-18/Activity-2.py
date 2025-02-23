afd20ab9d38845daee3eb5f4fb05f3a794bf194c
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-02-23 09:29:36 +0530

    Adding Lessom-18

---
+++ Lessons/Lesson-18/Activity-2.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+def getRandomDate(startDate,endDate):
+    print("Printing a random date in between",startDate,"and",endDate)
+    randomGenerator=random.random()
+    dateformat= '%M/%D/%Y'
+    startTime= time.mktime(time.strptime(startDate,dateformat))
+    endTime=time.mktime(time.strptime(endDate,dateformat))
+
+    randomTime= startTime+randomGenerator*(endTime-startTime)
+    randomDate= time.strptime(dateformat, time.localtime(randomTime))
+    return randomDate
+print("Random date=",getRandomDate("1/1/2016","12/12/2018"))
