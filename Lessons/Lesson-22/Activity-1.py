8af1af34d8635d0597811747961e7f2588e8f87f
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-03-09 09:32:56 +0530

    Adding Lesson-22

---
+++ Lessons/Lesson-22/Activity-1.py
@@ -0,0 +1,18 @@
+my_set={1,2,3,4,5}
+print(my_set)
+
+my_set1={1.0,"Hello",(1,2,3)}
+print(my_set1)
+
+my_set2={1,2,3,4,3,4}
+print(my_set2)
+
+my_set3=set([1,2,3,2])
+print(my_set3,"\n")
+
+num_set=set([0,1,3,4,5])
+print("Original Set:")
+print(num_set)
+num_set.pop()
+print("After removing the first element  from the said set:")
+print(num_set,"\n")
