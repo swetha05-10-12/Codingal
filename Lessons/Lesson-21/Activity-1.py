d4b166573638aad29053c17c808728d1c5cd53e6
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-03-10 18:13:21 +0530

    Adding Lesson-21

--- Lessons/Lesson-21/Activity-1.py
+++ Lessons/Lesson-21/Activity-1.py
@@ -1,28 +1,30 @@
-student_data={'id1'
-    {'name':['Sara'],
-     'class':['v'],
-     'subject_integration':['english,math,science']
-     },
-    'id2':
-    {'name':['Sara'],
-     'class':['v'],
-     'subject_integration':['english,math,science']
-     },
-    'id3':
-    {'name': ['Sara'],
-     'class':['v'],
-     'subject_integration[english,math,science]'
-     },
-     'id4':
-    {'name': ['Surya'],
-     'class':['v'],
-     'subject_integration[english,math,science]'
-     },
+student_data = {
+    'id1': {
+        'name': ['Sara'],
+        'class': ['v'],
+        'subject_integration': ['english', 'math', 'science']
+    },
+    'id2': {
+        'name': ['Sara'],
+        'class': ['v'],
+        'subject_integration': ['english', 'math', 'science']
+    },
+    'id3': {
+        'name': ['Sara'],
+        'class': ['v'],
+        'subject_integration': ['english', 'math', 'science']
+    },
+    'id4': {
+        'name': ['Surya'],
+        'class': ['v'],
+        'subject_integration': ['english', 'math', 'science']
+    }
 }
 
-result={}
+result = {}
 
-for key,value in student_data.items():
-    if value not  in result.values():
-        result[key]=value
+for key, value in student_data.items():
+    if value not in result.values():
+        result[key] = value
+
 print(result)
