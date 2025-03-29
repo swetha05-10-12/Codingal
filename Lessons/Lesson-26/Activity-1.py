246ee4fbd2d5bd92e4d9aff2d5b11d3c9389b61a
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-03-29 16:30:44 +0530

    Adding Lesson-26

--- Lessons/Lesson-26/Activity-1.py
+++ Lessons/Lesson-26/Activity-1.py
@@ -1,7 +1,13 @@
-class Student:
-    grade=8
-    print("Hi I am a student of grade",grade)
- 
-ob=Student()
+class IOString():
+    def __init__(self):
+        self.str1=""
+    def get_string(self):
+         self.str1=input("Enter a string:")
+    def print_string(self):
+        print("Result is :", self.str1.upper())
 
+str1=IOString()
 
+str1.get_string()
+str1.print_string()
+         
