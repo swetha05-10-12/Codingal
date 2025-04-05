e8da1ec303d09474efa4301748d9886f169b2d93
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-04-05 16:31:26 +0530

    Adding lesson-28

---
+++ Lessons/Lesson-28/Activity-3.py
@@ -0,0 +1,10 @@
+class point:
+    def __init__(self,x=0,y=0):
+        self.x=x
+        self.y=y
+
+    def __str__(self):
+        return "({0},{1})".format(self.x,self.y)
+    
+p1=point(4,6)
+print(p1)
