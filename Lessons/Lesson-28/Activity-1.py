5cf3696ef9fb19104429a1f3689f7f4abbab1dec
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-04-05 16:16:52 +0530

    Adding Lesson-28

---
+++ Lessons/Lesson-28/Activity-1.py
@@ -0,0 +1,13 @@
+class My_class:
+    __privateVar=27
+
+    def privMeth(self):
+        print("I am inside my class")
+
+    def hello(self):
+        print("Private Variable Value:",My_class.__privateVar)
+
+foo=My_class()
+foo.hello()
+foo.privMeth()
+
