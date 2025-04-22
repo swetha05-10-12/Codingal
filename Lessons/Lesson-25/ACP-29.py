dd674435ee32458a30070563870c371166993c04
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-04-22 12:29:38 +0530

    Adding ACP-29

---
+++ Lessons/Lesson-25/ACP-29.py
@@ -0,0 +1,20 @@
+class Dog:
+   
+    animal = "Dog"
+
+    def __init__(self, breed, colour):
+        self.breed = breed
+        self.colour = colour
+
+    def display_details(self):
+        print(f"Animal: {Dog.animal}")
+        print(f"Breed: {self.breed}")
+        print(f"Colour: {self.colour}")
+        print("-" * 20)
+
+
+dog1 = Dog("Labrador", "Golden")
+dog2 = Dog("Beagle", "Tricolor")
+
+dog1.display_details()
+dog2.display_details()
