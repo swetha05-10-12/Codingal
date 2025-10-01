7df401ea799df35e7a72ee354ccfcc8b18f1fca0
swetha05-10-12 <iam.swethaashok@gmail.com>
2025-10-01 10:58:04 +0530

    Fruits list

---
+++ Practice/ICT-School/ICT practicals (practice)-- /Fruit list.py
@@ -0,0 +1,20 @@
+fruits=["Apple", "Mango", "Banana", "Papaya", "Cherry", "Grapes", "Peach", "Plum"]
+
+print(fruits)
+print(fruits[1],fruits[6])
+fruits.append("Orange")
+
+for i in fruits:
+    print(i)
+
+print(fruits[0].upper())
+
+for i in fruits:
+    if i[0]=="a" or 0=="A":
+        print(i)
+
+for i in fruits:
+    if len(i)>5:
+        print (i)
+
+    
